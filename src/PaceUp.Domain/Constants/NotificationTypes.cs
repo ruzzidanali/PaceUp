44ba86db24a126db6a7ab8e56355d809752f0f5a
Ruzzidan Ali <ruzzidan25@gmail.com>
2026-09-08 11:03:50 +0800

    feat: add activity comments

--- src/PaceUp.Domain/Constants/NotificationTypes.cs
+++ src/PaceUp.Domain/Constants/NotificationTypes.cs
@@ -3,16 +3,16 @@ namespace PaceUp.Domain.Constants;
 public static class NotificationTypes
 {
     public const string NewFollower = "NewFollower";
-
     public const string ChallengeJoined = "ChallengeJoined";
-
     public const string ActivityKudos = "ActivityKudos";
+    public const string ActivityComment = "ActivityComment";
 
     public static bool IsValid(string? type)
     {
         return type is
             NewFollower or
             ChallengeJoined or
-            ActivityKudos;
+            ActivityKudos or
+            ActivityComment;
     }
 }
