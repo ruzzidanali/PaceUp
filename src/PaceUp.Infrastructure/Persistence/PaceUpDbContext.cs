998778938a7552d15e4fbc1196c71dfb2c55aa72
Ruzzidan Ali <ruzzidan25@gmail.com>
2026-09-08 11:03:45 +0800

    feat: add activity comments

--- src/PaceUp.Infrastructure/Persistence/PaceUpDbContext.cs
+++ src/PaceUp.Infrastructure/Persistence/PaceUpDbContext.cs
@@ -4,54 +4,27 @@ using PaceUp.Domain.Entities;
 
 namespace PaceUp.Infrastructure.Persistence;
 
-public class PaceUpDbContext :
-    DbContext,
-    IApplicationDbContext
+public class PaceUpDbContext : DbContext, IApplicationDbContext
 {
-    public PaceUpDbContext(
-        DbContextOptions<PaceUpDbContext> options)
-        : base(options)
-    {
-    }
+    public PaceUpDbContext(DbContextOptions<PaceUpDbContext> options) : base(options) { }
 
     public DbSet<User> Users => Set<User>();
     public DbSet<Activity> Activities => Set<Activity>();
-
     public DbSet<Goal> Goals => Set<Goal>();
+    public DbSet<UserIdentity> UserIdentities => Set<UserIdentity>();
+    public DbSet<EmailVerificationToken> EmailVerificationTokens => Set<EmailVerificationToken>();
+    public DbSet<PasswordResetToken> PasswordResetTokens => Set<PasswordResetToken>();
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+    public DbSet<Follow> Follows => Set<Follow>();
+    public DbSet<Notification> Notifications => Set<Notification>();
+    public DbSet<Kudos> Kudos => Set<Kudos>();
+    public DbSet<Comment> Comments => Set<Comment>();
+    public DbSet<Challenge> Challenges => Set<Challenge>();
+    public DbSet<ChallengeParticipant> ChallengeParticipants => Set<ChallengeParticipant>();
 
-    public DbSet<UserIdentity> UserIdentities =>
-        Set<UserIdentity>();
-
-    public DbSet<EmailVerificationToken> EmailVerificationTokens =>
-        Set<EmailVerificationToken>();
-
-    public DbSet<PasswordResetToken> PasswordResetTokens =>
-        Set<PasswordResetToken>();
-
-    public DbSet<RefreshToken> RefreshTokens =>
-        Set<RefreshToken>();
-
-    public DbSet<Follow> Follows =>
-        Set<Follow>();
-
-    public DbSet<Notification> Notifications =>
-        Set<Notification>();
-
-    public DbSet<Kudos> Kudos =>
-    Set<Kudos>();
-
-    public DbSet<Challenge> Challenges =>
-        Set<Challenge>();
-
-    public DbSet<ChallengeParticipant> ChallengeParticipants =>
-            Set<ChallengeParticipant>();
-
-    protected override void OnModelCreating(
-        ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.ApplyConfigurationsFromAssembly(
-            typeof(PaceUpDbContext).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PaceUpDbContext).Assembly);
     }
 }
