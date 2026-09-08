8db804da70485dee85e5275a4f7eead8971b19c1
Ruzzidan Ali <ruzzidan25@gmail.com>
2026-09-08 11:03:41 +0800

    feat: add activity comments

--- src/PaceUp.Application/Abstractions/Persistence/IApplicationDbContext.cs
+++ src/PaceUp.Application/Abstractions/Persistence/IApplicationDbContext.cs
@@ -6,29 +6,18 @@ namespace PaceUp.Application.Abstractions.Persistence;
 public interface IApplicationDbContext
 {
     DbSet<User> Users { get; }
-
     DbSet<UserIdentity> UserIdentities { get; }
-
     DbSet<Activity> Activities { get; }
-
     DbSet<PaceUp.Domain.Entities.Kudos> Kudos { get; }
-
+    DbSet<Comment> Comments { get; }
     DbSet<Goal> Goals { get; }
-
     DbSet<EmailVerificationToken> EmailVerificationTokens { get; }
-
     DbSet<PasswordResetToken> PasswordResetTokens { get; }
-
     DbSet<RefreshToken> RefreshTokens { get; }
-
     DbSet<Notification> Notifications { get; }
-
     DbSet<Follow> Follows { get; }
-
     DbSet<Challenge> Challenges { get; }
-
     DbSet<ChallengeParticipant> ChallengeParticipants { get; }
 
-    Task<int> SaveChangesAsync(
-        CancellationToken cancellationToken = default);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
